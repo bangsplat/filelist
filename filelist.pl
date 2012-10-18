756117081c4dd5188641a48515e8469d60cb5ba7
Bangsplat <theron@dankerton.net>
2012-10-17 17:47:55 -0700

    Version 0.9
    Added date/time to output file name, plus path/size/date filter
    functionality

--- filelist.pl
+++ filelist.pl
@@ -1,4 +1,4 @@
-#!perl
+#!/usr/bin/perl
 use strict;	# Enforce some good programming rules
 
 use Getopt::Long;
@@ -9,6 +9,8 @@ use Time::localtime;
 
 #
 # fileList.pl
+# 
+# version 0.9
 #
 # Create a list of files
 #
@@ -19,19 +21,23 @@ use Time::localtime;
 # --filter <regex>	Process only if filename matches regex
 # --output | -o <filename>	Output results to file	Default is files_YYYYMMDD.txt
 # 	<filename> = STDIO outputs to <STDIO>
-# --[no]path | -[no]P	Include full path in output	Default is to not include full path
-# --[no]size | -[no]S	Include file size in output	Default is to not include file size
-# --[no]date | -[no]D	Include file mod date in ouptut	Default is to not include file mod date
+# --[no]path	Include full path in output	Default is to not include full path
+# --[no]size	Include file size in output	Default is to not include file size
+# --[no]date	Include file mod date in ouptut	Default is to not include file mod date
 # --help | -?	Displays help message	Default is not to show help message
 # --[no]debug	Display debugging information	Default is no debug mode
 # --[no]test	Test mode - display file names but do not process	Default is no test mode
 #
 
 my ( $directory_param, $recurse_param, $help_param );
-my ( $debug_param, $test_param );
+my ( $version_param, $debug_param, $test_param );
 my ( $output_param, $filter_param, $path_param, $size_param, $date_param );
 my ( $output_filename, $output_string );
 my @MONTHS = qw( 01 02 03 04 05 06 07 08 09 10 11 12 );
+my @DAYS = qw( 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 );
+my @HOURS = qw( 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 );
+my @MINUTES = qw ( 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 );
+my @SECONDS = qw( 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 );
 
 GetOptions(	'directory|d=s'	=> \$directory_param,
 		'recurse|r!'	=> \$recurse_param,
@@ -39,14 +45,31 @@ GetOptions(	'directory|d=s'	=> \$directory_param,
 		'output|o=s'	=> \$output_param,
 		'path|P!'	=> \$path_param,
 		'size|S!'	=> \$size_param,
-		'date|D!'	=> \$date_param,
+		'date!'		=> \$date_param,
 		'help|?'	=> \$help_param,
+		'version'	=> \$version_param,
 		'debug!'	=> \$debug_param,
 		'test!'		=> \$test_param );
+
+
+if ( $debug_param ) {
+	print "passed parameters:\n";
+	print "directory_param: $directory_param\n";
+	print "recurse_param: $recurse_param\n";
+	print "output_param: $output_param\n";
+	print "path_param: $path_param\n";
+	print "size_param: $size_param\n";
+	print "date_param: $date_param\n";
+	print "version_param: $version_param\n";
+	print "debug_param: $debug_param\n";
+	print "test_param: $test_param\n";
+	print "\n";
+}
 
 # If user asked for help, display help message and exit
 if ( $help_param ) {
 	print "fileList.pl\n\n";
+	print "version 0.9\n\n";
 	print "Create a list of files\n\n";
 	print "Acceptable flags:\n\n";
 	print "--directory | -d [<directory>] - set starting directory\n";
@@ -58,13 +81,18 @@ if ( $help_param ) {
 	print "--output | -o <filename> - output to file <filename>\n";
 	print "If omitted, filelist_YYYYMMDD is created in starting directory\n";
 	print "Use --output STDIO to output to console\n\n";
-	print "--[no]path | -[no]P - use full path name\n";
+	print "--[no]path - use full path name\n";
 	print "Default is to not use full path name\n\n";
-	print "--[no]size | -[no]S - include file size\n";
+	print "--[no]size - include file size\n";
 	print "Default is to not include file size\n\n";
-	print "--[no]date | -[no]D - include file modification date\n";
+	print "--[no]date - include file modification date\n";
 	print "Default is to not include file modification date\n\n";
 	print "--help | -? - display this message\n\n";
+	exit;
+}
+
+if ( $version_param ) {
+	print "filelist.pl version 0.9\n";
 	exit;
 }
 
@@ -72,7 +100,7 @@ if ( $help_param ) {
 if ( $directory_param eq undef ) { $directory_param = cwd; }	# Current working directory
 if ( $recurse_param eq undef ) { $recurse_param = 0; }		# False
 if ( $output_param eq undef ) {
-	my $tempdate = localtime()->year + 1900 . $MONTHS[localtime()->mon] . localtime()->mday;
+	my $tempdate = localtime()->year + 1900 . $MONTHS[localtime()->mon] . $DAYS[localtime()->mday] . $HOURS[localtime()->hour] . $MINUTES[localtime()->min] . $SECONDS[localtime()->sec];
 	$output_param = $directory_param . "/filelist_" . $tempdate . ".txt";
 }
 if ( $path_param eq undef ) { $path_param = 0; }		# False
@@ -82,14 +110,17 @@ if ( $debug_param eq undef ) { $debug_param = 0; }		# False
 if ( $test_param eq undef ) { $test_param = 0; }		# False
 
 if ( $debug_param ) {
+	print "adjusted parameters:\n";
 	print "directory_param: $directory_param\n";
 	print "recurse_param: $recurse_param\n";
 	print "output_param: $output_param\n";
 	print "path_param: $path_param\n";
 	print "size_param: $size_param\n";
 	print "date_param: $date_param\n";
+	print "version_param: $version_param\n";
 	print "debug_param: $debug_param\n";
 	print "test_param: $test_param\n";
+	print "\n";
 }
 
 $output_string = "";			# Empty the output string
